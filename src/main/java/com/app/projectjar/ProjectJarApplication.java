2e5f8d958c20983f83dc43bac3fef31668fe9f36
LeeJunSang <miraclelee0613@gmail.com>
2023-04-13 10:57:06 +0900

    initialize

---
+++ src/main/java/com/app/projectjar/ProjectJarApplication.java
@@ -0,0 +1,13 @@
+package com.app.projectjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectJarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/projectjar/ProjectJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.projectjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
