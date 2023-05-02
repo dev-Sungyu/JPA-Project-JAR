4bfd0a475e231f18868128af7961ccde8e283b4a
dev-Sungyu <tjfdhs364@gmail.com>
2023-05-02 18:00:24 +0900

    중간 커밋

--- src/main/java/com/app/projectjar/entity/suggest/Suggest.java
+++ src/main/java/com/app/projectjar/entity/suggest/Suggest.java
@@ -1,6 +1,7 @@
 package com.app.projectjar.entity.suggest;
 
 import com.app.projectjar.audit.Period;
+import com.app.projectjar.entity.file.File;
 import com.app.projectjar.entity.member.Member;
 import com.app.projectjar.type.Document;
 import lombok.*;
@@ -21,10 +22,13 @@ public class Suggest extends Period {
     @Embedded
     private Document document;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "suggest", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
-    private List<SuggestReply> suggestReplies = new ArrayList<>();
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "suggest", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<SuggestReply> suggestReplies = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "suggest",cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
+    private List<File> files = new ArrayList<>();
 }
