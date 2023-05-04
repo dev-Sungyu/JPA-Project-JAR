49450ace6139b48f842e3964f511db4dfef40dba
Dev-PureIron <2sooncheol@gmail.com>
2023-05-04 17:11:37 +0900

    백단 작업 진행 중

--- src/main/java/com/app/projectjar/repository/member/MemberQueryDsl.java
+++ src/main/java/com/app/projectjar/repository/member/MemberQueryDsl.java
@@ -1,4 +1,27 @@
 package com.app.projectjar.repository.member;
 
 public interface MemberQueryDsl {
+
+//    이메일 중복 검사
+
+//    휴대폰 중복 검사
+
+//    회원 아이디 찾기
+
+//    회원 비밀번호 찾기
+
+//    로그인
+
+//    계정찾기
+
+//    비밀번호 변경
+
+//    계정상태 조회
+
+//    회원정보 조회
+
+//    회원정보 수정
+
+
+
 }
---
+++ src/test/java/com/app/projectjar/memberRepository/MemberRepositoryTests.java
@@ -0,0 +1,32 @@
+package com.app.projectjar.memberRepository;
+
+import com.app.projectjar.entity.member.Member;
+import com.app.projectjar.repository.member.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+@Slf4j
+public class MemberRepositoryTests {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void saveTest(){
+        Member member = new Member("user01@naver.com","qwer1234!","010-1234-5678","최규선","최팀장");
+        memberRepository.save(member);
+    }
+
+    @Test
+    public void findByIdTest(){
+        memberRepository.findById(1L).ifPresent();
+    }
+
+}
