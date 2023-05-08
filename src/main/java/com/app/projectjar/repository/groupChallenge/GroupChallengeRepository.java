b47ebdb99c45c7ae52ac794fa9b39987dec15469
WookCoding <dnrwhddla@gmail.com>
2023-05-08 19:23:08 +0900

    groupChallenge 목록, 상세보기 한방쿼리로 변경

--- src/main/java/com/app/projectjar/repository/groupChallenge/GroupChallengeRepository.java
+++ src/main/java/com/app/projectjar/repository/groupChallenge/GroupChallengeRepository.java
@@ -1,7 +1,14 @@
 package com.app.projectjar.repository.groupChallenge;
 
 import com.app.projectjar.entity.groupChallenge.GroupChallenge;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 public interface GroupChallengeRepository extends JpaRepository<GroupChallenge, Long>, GroupChallengeQueryDsl {
 }
