1a40c1ae6cc2d315f447ad57d94a09a70dc2e990
Dev-PureIron <2sooncheol@gmail.com>
2023-05-08 22:02:49 +0900

    작업 진행 중

--- src/main/java/com/app/projectjar/repository/member/MemberQueryDslImpl.java
+++ src/main/java/com/app/projectjar/repository/member/MemberQueryDslImpl.java
@@ -5,6 +5,7 @@ import com.app.projectjar.entity.challenge.QChallengeAttend;
 import com.app.projectjar.entity.groupChallenge.QGroupChallengeAttend;
 import com.app.projectjar.entity.member.Member;
 import com.app.projectjar.domain.dto.MemberDTO;
+import com.app.projectjar.entity.member.QMember;
 import com.app.projectjar.type.BadgeType;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -54,22 +55,23 @@ public class MemberQueryDslImpl implements MemberQueryDsl {
 //    멤버 디티오 정보 조회
     @Override
     public Optional<MemberDTO> findByMemberDTOId_QueryDSL(Long id) {
-        return Optional.ofNullable(
-                query.select(new QMemberDTO(
-                        member.id,
-                        member.memberEmail,
-                        member.memberPassword,
-                        member.memberPhoneNumber,
-                        member.memberName,
-                        member.memberNickname,
-                        member.memberStatus,
-                        member.badgeType
-                ))
-                        .from(member)
-                        .leftJoin(member.memberFile, memberFile)
-                        .where(member.id.eq(id))
-                        .fetchOne());
-//        return Optional.ofNullable(query.select(member).from(member).join(member.memberFile).fetchJoin().where(member.id.eq(memberId)).fetchOne())
+//        return Optional.ofNullable(
+//                query.select(new QMemberDTO(
+//                        member.id,
+//                        member.memberEmail,
+//                        member.memberPassword,
+//                        member.memberPhoneNumber,
+//                        member.memberName,
+//                        member.memberNickname,
+//                        member.memberStatus,
+//                        member.badgeType
+//                ))
+//                        .from(member)
+//                        .leftJoin(member.memberFile, memberFile)
+//                        .where(member.id.eq(id))
+//                        .fetchOne());
+//        return Optional.ofNullable(query.select(member.memberFile).from(member).join(member.memberFile).fetchJoin().where(member.id.eq(id)).fetchOne());
+        return null;
     }
 
 //    멤버 정보 조회
@@ -79,6 +81,20 @@ public class MemberQueryDslImpl implements MemberQueryDsl {
                 from(member).
                 where(member.id.eq(id)).fetchOne());
     }
+
+    //    멤버 정보 조회(파일 들어가는 버전)
+//    @Override
+//    public Optional<Member> findByMemberId_QueryDsl2(Long id) {
+//        Member member = query.select(QueryMember.member)
+//                .from(QMember.member)
+//                .leftJoin(QMember.member.memberFile)
+//                .fetchJoin()
+//                .where(QMember.member.id.eq(id))
+//                .fetchOne();
+//
+//        return Optional.ofNullable(member);
+//    }
+
 
 //    회원 정보 수정
     @Override
@@ -117,7 +133,7 @@ public class MemberQueryDslImpl implements MemberQueryDsl {
 
     }
 
-    //    뱃지 수정
+//    뱃지 수정
     @Override
     public void updateMemberBadge_QueryDSL(Long id, BadgeType badgeType) {
 
@@ -133,26 +149,27 @@ public class MemberQueryDslImpl implements MemberQueryDsl {
 
         int totalCount = countPersonal.intValue() + countGroup.intValue();
 
-        BadgeType newBadgeType = badgeType;
+//        BadgeType newBadgeType = badgeType;
+//
+//        if (totalCount >= 10) {
+//            newBadgeType = BadgeType.ONE;
+//
+//            if (totalCount >= 20) {
+//                newBadgeType = BadgeType.TWO;
+//            }
+//            if (totalCount >= 30) {
+//                newBadgeType = BadgeType.THREE;
+//            }
+//        } else {
+//            newBadgeType = BadgeType.ZERO;
+//        }
+//        if (newBadgeType != badgeType) {
 
-        if (totalCount >= 10) {
-            newBadgeType = BadgeType.ONE;
-
-            if (totalCount >= 20) {
-                newBadgeType = BadgeType.TWO;
-            }
-            if (totalCount >= 30) {
-                newBadgeType = BadgeType.THREE;
-            }
-        } else {
-            newBadgeType = BadgeType.ZERO;
-        }
-        if (newBadgeType != badgeType) {
+//        연산은 서비스에서 진행하기
             query.update(member)
-                    .set(member.badgeType, newBadgeType)
+                    .set(member.badgeType, badgeType)
                     .where(member.id.eq(id))
                     .execute();
-        }
     }
 
 }
