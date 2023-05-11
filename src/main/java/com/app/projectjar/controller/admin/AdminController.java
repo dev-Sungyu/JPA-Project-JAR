e12887cd422561007caaed7002c832f992417a8b
dev-Sungyu <tjfdhs364@gmail.com>
2023-05-12 02:33:51 +0900

    중간 커밋

--- src/main/java/com/app/projectjar/controller/admin/AdminController.java
+++ src/main/java/com/app/projectjar/controller/admin/AdminController.java
@@ -1,14 +1,31 @@
 package com.app.projectjar.controller.admin;
 
+import com.app.projectjar.domain.notice.NoticeDTO;
+import com.app.projectjar.domain.page.PageDTO;
+import com.app.projectjar.service.member.MemberService;
+import com.app.projectjar.service.notice.NoticeService;
+import com.app.projectjar.service.suggest.SuggestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/admin/*")
 @RequiredArgsConstructor
 public class AdminController {
+    private final SuggestService suggestService;
+    private final NoticeService noticeService;
+    private final MemberService memberService;
+
+
     @GetMapping("board/challenge/detail")
     public void adminChallengeDetail() {}
     @GetMapping("board/challenge/list")
@@ -31,10 +48,22 @@ public class AdminController {
     public void adminMemberList() {}
     @GetMapping("member/modify")
     public void adminMemberModify() {}
-    @GetMapping("board/notice/detail")
-    public void adminNoticeDetail() {}
+    @GetMapping("board/notice/detail/{noticeId}")
+    public String adminNoticeDetail(Model model, @PathVariable("noticeId") Long noticeId) {
+        NoticeDTO noticeDTO = noticeService.getNotice(noticeId);
+
+        model.addAttribute("noticeDTOS", noticeDTO);
+
+        return "admin/board/notice/detail";
+    }
     @GetMapping("board/notice/list")
-    public void adminNoticeList() {}
+    public String adminNoticeList(Model model, @RequestParam(value="page", defaultValue="1") int page) {
+        Page<NoticeDTO> noticePage = noticeService.getAllNoticesWithPaging(page - 1);
+        List<String> noticeTitles = noticePage.stream().map(NoticeDTO::getNoticeTitle).collect(Collectors.toList());
+        model.addAttribute("pageDTO",new PageDTO(noticePage));
+        model.addAttribute("noticeDTOS", noticePage.getContent());
+        return "admin/board/notice/list";
+    }
     @GetMapping("board/notice/modify")
     public void adminNoticeModify() {}
     @GetMapping("board/notice/write")
