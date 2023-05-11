0a512a1ec03268835baab3a198a7b2ff2fd5d864
WookCoding <dnrwhddla@gmail.com>
2023-05-11 23:58:16 +0900

    댓글 작업중

--- src/main/java/com/app/projectjar/domain/reply/ReplyDTO.java
+++ src/main/java/com/app/projectjar/domain/reply/ReplyDTO.java
@@ -1,14 +1,16 @@
-package com.app.projectjar.domain;
+package com.app.projectjar.domain.reply;
 
 import com.app.projectjar.domain.member.MemberDTO;
 import com.app.projectjar.domain.suggest.SuggestDTO;
 import com.app.projectjar.type.BadgeType;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.*;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
-@Getter @ToString
+@Component
+@Data
 @NoArgsConstructor
 public class ReplyDTO {
 
@@ -26,5 +28,13 @@ public class ReplyDTO {
         this.registerDate = registerDate;
         this.memberDTO = memberDTO;
         this.suggestDTO = suggestDTO;
+    }
+
+    @Builder
+    public ReplyDTO(Long id, String replyContent, LocalDateTime registerDate, MemberDTO memberDTO) {
+        this.id = id;
+        this.replyContent = replyContent;
+        this.registerDate = registerDate;
+        this.memberDTO = memberDTO;
     }
 }
