37e10c5d19f09b9d103827e767d7286dd67214f6
WookCoding <dnrwhddla@gmail.com>
2023-05-15 20:56:10 +0900

    diary : repo, service,

--- src/main/java/com/app/projectjar/repository/personalChallenge/PersonalChallengeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.projectjar.repository.personalChallenge;
-
-import com.app.projectjar.entity.personalChallenge.PersonalChallenge;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonalChallengeRepository extends JpaRepository<PersonalChallenge, Long>, PersonalQueryDsl {
-}
