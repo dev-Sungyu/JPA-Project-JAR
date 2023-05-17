4aa55ba9691f8091eacb6b2fc31899dc1ba968d8
WookCoding <dnrwhddla@gmail.com>
2023-05-17 15:25:32 +0900

    challengeAttendRestController 완료

--- src/main/java/com/app/projectjar/controller/board/challenge/reply/ChallengeReplyRestController.java
+++ src/main/java/com/app/projectjar/controller/board/challenge/reply/ChallengeReplyRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/challenge/reply/*")
-public class ChallengeReplyController {
+public class ChallengeReplyRestController {
 
     private final ChallengeReplyService challengeReplyService;
 
