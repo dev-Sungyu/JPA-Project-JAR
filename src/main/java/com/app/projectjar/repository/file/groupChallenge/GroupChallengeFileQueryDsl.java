80f76fe0e46667e9e26ad4f0a258b7eccd08f95e
dev-Sungyu <tjfdhs364@gmail.com>
2023-05-17 11:13:36 +0900

    관리자 페이지 작성하기 서비스까지 완료

--- src/main/java/com/app/projectjar/repository/file/groupChallenge/GroupChallengeFileQueryDsl.java
+++ src/main/java/com/app/projectjar/repository/file/groupChallenge/GroupChallengeFileQueryDsl.java
@@ -1,9 +1,9 @@
 package com.app.projectjar.repository.file.groupChallenge;
 
+import com.app.projectjar.domain.file.FileDTO;
 import com.app.projectjar.entity.file.groupChallenge.GroupChallengeFile;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,4 +14,6 @@ public interface GroupChallengeFileQueryDsl {
 
     // 그룹 챌린지 파일 여러개 조회 ( 게시판 상세보기에서 사용 )
 //    public List<GroupChallengeFile> findAllByGroupChallengeFileId_QueryDsl(Long groupChallengeId);
+
+    public List<FileDTO> findAllFiles(LocalDateTime date);
 }
