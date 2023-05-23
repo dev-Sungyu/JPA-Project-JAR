d6351970305d93a91255b22bfc144d0ee39f3845
WookCoding <dnrwhddla@gmail.com>
2023-05-23 16:30:53 +0900

    getMapping model 객체 추가

--- src/main/java/com/app/projectjar/controller/board/diary/DiaryController.java
+++ src/main/java/com/app/projectjar/controller/board/diary/DiaryController.java
@@ -1,8 +1,11 @@
 package com.app.projectjar.controller.board.diary;
 
 import com.app.projectjar.domain.diary.DiaryDTO;
+import com.app.projectjar.domain.member.MemberDTO;
 import com.app.projectjar.service.diary.DiaryService;
+import com.app.projectjar.service.mypage.MyPageService;
 import lombok.RequiredArgsConstructor;
+import org.dom4j.rule.Mode;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Controller;
@@ -10,15 +13,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/board/diary/*")
 @RequiredArgsConstructor
 public class DiaryController {
     private final DiaryService diaryService;
+    private final MyPageService myPageService;
 
 
     @GetMapping("list")
-    public void goToList(){
+    public void goToList(Model model, HttpSession session){
+        Long memberId = ((MemberDTO)session.getAttribute("member")).getId();
+        MemberDTO memberDTO = myPageService.getMemberDTO(memberId);
+
+        model.addAttribute("memberDTO",memberDTO);
     }
 
     @GetMapping("list-content")
@@ -29,17 +39,24 @@ public class DiaryController {
     }
 
     @GetMapping("detail/{diaryId}")
-    public String goToDetail(Model model, @PathVariable("diaryId") Long diaryId) {
+    public String goToDetail(Model model, @PathVariable("diaryId") Long diaryId, HttpSession session) {
+        Long memberId = ((MemberDTO)session.getAttribute("member")).getId();
+        MemberDTO memberDTO = myPageService.getMemberDTO(memberId);
 
         DiaryDTO diaryDTO = diaryService.getDiary(diaryId);
+
+        model.addAttribute("memberDTO",memberDTO);
         model.addAttribute("diaryDTO", diaryDTO);
         return "/board/diary/detail";
     }
 
     @GetMapping("modify/{diaryId}")
-    public String goToModify(Model model, @PathVariable("diaryId") Long diaryId) {
+    public String goToModify(Model model, @PathVariable("diaryId") Long diaryId, HttpSession session) {
+        Long memberId = ((MemberDTO)session.getAttribute("member")).getId();
+        MemberDTO memberDTO = myPageService.getMemberDTO(memberId);
 
         DiaryDTO diaryDTO = diaryService.getDiary(diaryId);
+        model.addAttribute("memberDTO",memberDTO);
         model.addAttribute("diaryDTO", diaryDTO);
         return "/board/diary/modify";
     }
